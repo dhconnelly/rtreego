700906ce33570192926c2341a1b5b88cb75ff82f
Daniel Connelly <dhconnelly@gmail.com>
2012-02-08 22:44:21 -0500

    adding doc and tests for point, dist, rect, and newrect

--- geom.go
+++ geom.go
@@ -5,8 +5,16 @@ import (
 	"math"
 )
 
+// Point represents a point in n-dimensional Euclidean space.
 type Point []float64
 
+// Rect represents a subset of n-dimensional Euclidean space of the form
+// [a1, b1] x [a2, b2] x ... x [an, bn], where ai < bi for all 1 <= i <= n.
+type Rect struct {
+	p, q Point // p[i] <= q[i] for all i
+}
+
+// DimError represents a failure due to mismatched dimensions.
 type DimError struct {
 	Expected int
 	Actual int
@@ -16,6 +24,8 @@ func (err *DimError) Error() string {
 	return fmt.Sprintf("Expected dim %d, got %d", err.Expected, err.Actual)
 }
 
+// Dist computes the Euclidean distance between two points p and q.  When
+// len(p) != len(q), a pointer to a DimError instance is returned.
 func Dist(p, q Point) (float64, error) {
 	if len(p) != len(q) {
 		return 0, &DimError{len(p), len(q)}
@@ -26,4 +36,19 @@ func Dist(p, q Point) (float64, error) {
 		sum += dx * dx
 	}
 	return math.Sqrt(sum), nil
+}
+
+// NewRect constructs and returns a pointer to a Rect given a corner point and
+// the lengths of each dimension.  The point p should be the most-negative point
+// on the rectangle (in every dimension).  If len(lengths) != len(p), then a
+// pointer to a DimError instance is returned.
+func NewRect(p Point, lengths []float64) (*Rect, error) {
+	if len(p) != len(lengths) {
+		return nil, &DimError{len(p), len(lengths)}
+	}
+	q := make([]float64, len(p))
+	for i := range p {
+		q[i] = p[i] + lengths[i]
+	}
+	return &Rect{p, q}, nil
 }
--- geom_test.go
+++ geom_test.go
@@ -5,29 +5,46 @@ import (
 	"testing"
 )
 
-type distTest struct {
-	p1, p2 Point
-	out float64
-	err error
-}
-
-var distTests = []distTest{
-	distTest{Point{1, 2, 3}, Point{3, 4, 5}, math.Sqrt(12.0), nil},
-	distTest{Point{1, 2, 3}, Point{3, 4}, 0, &DimError{3, 2}},
-	distTest{Point{1, 2}, Point{3, 4, 5}, 0, &DimError{2, 3}},
-}
-
 func TestDist(t *testing.T) {
-	for _, dt := range distTests {
-		d, err := Dist(dt.p1, dt.p2)
-		if d != dt.out {
-			t.Errorf("Expected %v, got %v", d, dt.out)
+	p := Point{1, 2, 3}
+	q := Point{4, 5, 6}
+	r := Point{7, 8}
+	dist := math.Sqrt(27)
+
+	if d, err := Dist(p, q); err != nil || d != dist {
+		t.Errorf("Dist(%v, %v) = %v; expected %v", p, q, d, dist)
+	}
+
+	if d, err := Dist(p, r); d != 0 || err == nil {
+		t.Errorf("Expected failure on Dist(%v, %v); got %v", p, r, d)
+	}
+}
+
+func TestNewRect(t *testing.T) {
+	p := Point{1.0, -2.5, 3.0}
+	q := Point{3.5, 5.5, 4.5}
+	lengths := []float64{2.5, 8.0, 1.5}
+
+	rect, err := NewRect(p, lengths)
+	if err != nil {
+		t.Errorf("Unexpected failure on NewRect(%v, %v)", p, lengths)
+	}
+	
+	for i, p1 := range p {
+		p2 := rect.p[i]
+		if p1 != p2 {
+			t.Errorf("Expected rect.s[i] = %v; got %v", p1, p2)
 		}
 
-		e1, ok1 := dt.err.(*DimError)
-		e2, ok2 := err.(*DimError)
-		if (ok1 != ok2) || (ok1 && *e1 != *e2) {
-			t.Errorf("Expected '%v', got '%v'", e1, e2)
+		q1 := q[i]
+		q2 := rect.q[i]
+		if q1 != q2 {
+			t.Errorf("Expected rect.s[i] = %v; got %v", q1, q2)
 		}
+	}
+
+	r := Point{-7.0, 10.0}
+	if rect2, err := NewRect(r, lengths); rect2 != nil || err == nil {
+		t.Errorf("Expected failure on NewRect(%v, %v); got %v", r, lengths, rect2)
 	}
 }
