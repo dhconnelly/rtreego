903dbb353be15dad0ee594f8f2f4ed0ba996f65e
Daniel Connelly <dhconnelly@gmail.com>
2012-02-09 21:28:27 -0500

    added ContainsPoint and tests; some test refactoring

--- geom_test.go
+++ geom_test.go
@@ -9,16 +9,20 @@ import (
 	"testing"
 )
 
+const EPS = 0.000000001
+
 func TestDist(t *testing.T) {
 	p := Point{1, 2, 3}
 	q := Point{4, 5, 6}
-	r := Point{7, 8}
 	dist := math.Sqrt(27)
-
 	if d, err := Dist(p, q); err != nil || d != dist {
 		t.Errorf("Dist(%v, %v) = %v; expected %v", p, q, d, dist)
 	}
+}
 
+func TestDistDimMismatch(t *testing.T) {
+	p := Point{1, 2, 3}
+	r := Point{7, 8}
 	if d, err := Dist(p, r); d != 0 || err == nil {
 		t.Errorf("Expected failure on Dist(%v, %v); got %v", p, r, d)
 	}
@@ -31,24 +35,59 @@ func TestNewRect(t *testing.T) {
 
 	rect, err := NewRect(p, lengths)
 	if err != nil {
-		t.Errorf("Unexpected failure on NewRect(%v, %v)", p, lengths)
+		t.Errorf("Error on NewRect(%v, %v): %v", p, lengths, err)
+	}
+	if d, _ := Dist(p, rect.p); d > EPS {
+		t.Errorf("Expected p == rect.p")
+	}
+	if d, _ := Dist(q, rect.q); d > EPS {
+		t.Errorf("Expected q == rect.q")
 	}
 
-	for i, p1 := range p {
-		p2 := rect.p[i]
-		if p1 != p2 {
-			t.Errorf("Expected rect.s[i] = %v; got %v", p1, p2)
-		}
+}
 
-		q1 := q[i]
-		q2 := rect.q[i]
-		if q1 != q2 {
-			t.Errorf("Expected rect.s[i] = %v; got %v", q1, q2)
-		}
+func TestNewRectDimMismatch(t *testing.T) {
+	p := Point{-7.0, 10.0}
+	lengths := []float64{2.5, 8.0, 1.5}
+	_, err := NewRect(p, lengths)
+	if _, ok := err.(*DimError); !ok {
+		t.Errorf("Expected DimError on NewRect(%v, %v)", p, lengths)
+	}
+}
+
+func TestNewRectDistError(t *testing.T) {
+	p := Point{1.0, -2.5, 3.0}
+	lengths := []float64{2.5, -8.0, 1.5}
+	_, err := NewRect(p, lengths)
+	if _, ok := err.(*DistError); !ok {
+		t.Errorf("Expected DistError on NewRect(%v, %v)", p, lengths)
+	}
+}
+
+func TestContainsPoint(t *testing.T) {
+	p := Point{3.7, -2.4, 0.0}
+	lengths := []float64{6.2, 1.1, 4.9}
+	rect, _ := NewRect(p, lengths)
+
+	q := Point{4.5, -1.7, 4.8}
+	if yes, err := rect.ContainsPoint(q); !yes || err != nil {
+		t.Errorf("Expected %v contains %v", rect, q)
 	}
 
-	r := Point{-7.0, 10.0}
-	if rect2, err := NewRect(r, lengths); rect2 != nil || err == nil {
-		t.Errorf("Expected failure on NewRect(%v, %v); got %v", r, lengths, rect2)
+	r := Point{4.5, -1.7, -3.2}
+	if yes, err := rect.ContainsPoint(r); yes || err != nil {
+		t.Errorf("Expected %v doesn't contain %v", rect, q)
+	}
+}
+
+func TestContainsPointDimError(t *testing.T) {
+	p := Point{3.7, -2.4, 0.0}
+	lengths := []float64{6.2, 1.1, 4.9}
+	rect, _ := NewRect(p, lengths)
+
+	q := Point{4.5, -1.7}
+	_, err := rect.ContainsPoint(q)
+	if _, ok := err.(*DimError); !ok {
+		t.Errorf("Expected DimError on %v.ContainsPoint(%v)", rect, q)
 	}
 }
