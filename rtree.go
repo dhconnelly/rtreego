305af478378d6a2d0e395f1e5f1c0ab63eab1280
tsne <tsne.dev@outlook.com>
2018-03-20 16:25:08 +0100

    add comment for integer ceiling

--- rtree.go
+++ rtree.go
@@ -91,7 +91,7 @@ func (s *dimSorter) Less(i, j int) bool {
 // walkPartitions splits objs into slices of maximum k elements and
 // iterates over these partitions.
 func walkPartitions(k int, objs []entry, iter func(parts []entry)) {
-	n := (len(objs) + k - 1) / k
+	n := (len(objs) + k - 1) / k // ceil(len(objs)/ k)
 
 	for i := 1; i < n; i++ {
 		iter(objs[(i-1)*k : i*k])
